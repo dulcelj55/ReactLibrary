77691fc15c1bdafb5e8228f2c22b52682791d1cf
Dulce Jaramillo <djaramillo@LABA.PSWAS.LOCAL>
2023-09-19 18:56:30 -0700

    break to go home

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import { useState } from 'react'
+import './index.css'
+import './App.css'
+import Sidebar from './Component/Sidebar'
+
+function App() {
+  
+
+  return (
+    <>
+     <div id="app-container">
+     <Sidebar/>
+    <div className="page-container">
+    
+    </div>
+     </div>
+    </>
+  )
+}
+
+export default App
---
+++ src/Component/Sidebar/index.jsx
@@ -0,0 +1,15 @@
+
+import "/.index.css"
+
+const Sidebar = () => {
+  return (
+    <aside>
+        <button>Login/Create</button>
+        <button>New Book</button>
+        <button>View Books</button>
+        <button>LightSwitch</button>
+    </aside>
+  )
+}
+
+export default Sidebar
